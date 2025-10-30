422295723e9c8915bb40412e5809fd59f7303c83
shahd <mazouzchahd12@gmail.com>
2025-10-30 17:25:11 +0100

    using bootstrap and css as well s/@popperjs/core/dist/esm/dom-utils/getLayoutRect.js ²#	new file:   node_modules/@popperjs/core/dist/esm/dom-utils/getNodeName.js

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
-function myfunction(){
-    const output = document.getElementById("numb");
-    output.textContent = "click here";
+
+    function myfunction(){
+    
+    const selected = document.querySelector('input[type=radio]:checked');
+   
+        const LOAN = document.getElementById("LOAN");
+        LOAN.style.display = 'flex';
+}
+function calcule(){
+    const number = document.getElementById("loan").value
+    const numbe = document.getElementById("duration").value
+    const numb = document.getElementById("salary").value
+    console.log(number)
+    console.log(numbe)
+    console.log(numb)
 }
