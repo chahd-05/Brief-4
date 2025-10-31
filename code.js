ca9fafd8960f981b65b66e68c1b2f3d6d5f0eac8
shahd <mazouzchahd12@gmail.com>
2025-10-31 12:13:00 +0100

    feat : add javascript et modification in code

--- code.js
+++ code.js
@@ -1,16 +1,72 @@
 
-    function myfunction(){
-    
-    const selected = document.querySelector('input[type=radio]:checked');
-   
-        const LOAN = document.getElementById("LOAN");
-        LOAN.style.display = 'flex';
+const afish = document.getElementById("parag");
+const loantype = document.querySelectorAll(".bank");
+const calcul = document.querySelector(".bob");
+const title = document.getElementById("option");
+const LOAN = document.getElementById("LOAN");
+const output1 = document.getElementById("parag");
+const output2 = document.getElementById("para");
+const output3 = document.getElementById("par");
+const output4 = document.getElementById("pa");
+const output5 = document.getElementById("pag");
+let rate = 0;
+let selectedtype = "";
+loantype.forEach(Button =>{
+    Button.addEventListener("click", e => {
+    console.log(e.target.value)
+    const type = e.target.value
+    LOAN.style.display = 'flex';
+    title.textContent = type;
+    selectedtype = type;
+    return type;
+  
+    })
+
 }
-function calcule(){
-    const number = document.getElementById("loan").value
-    const numbe = document.getElementById("duration").value
-    const numb = document.getElementById("salary").value
-    console.log(number)
-    console.log(numbe)
-    console.log(numb)
+
+)
+    
+function secondfunction(){
+    const border = document.getElementById("fin")
+const loan = Number(document.getElementById("loan").value);
+const duration = Number(document.getElementById("duration").value);
+const salary= Number(document.getElementById("salary").value);
+border.style.display = 'block'
+    switch(selectedtype){
+
+        case "house": rate = 0.045
+            break;
+
+        case "appartment": rate = 0.05
+            break;
+
+            case "land": rate = 0.06
+            break;
+
+            case "business": rate = 0.07
+            break;
+
+            case "personal": rate = 0.08
+            break;
+            }
+    t = rate/12
+    p = Math.pow(t+1 , duration)
+    M = loan * (t * p) / (p - 1)
+    totalinterest = (M * duration) - loan
+    repayment = (M * duration)
+    if(M <= salary * 0.4){
+        
+    output1.textContent = selectedtype
+    output2.textContent = loan
+    output3.textContent = M.toFixed(2)
+    output4.textContent = totalinterest.toFixed(2)
+    output5.textContent = repayment.toFixed(2)
+}
+else {
+    output1.textContent = "the loan is not available with this income."
+    output2.textContent = ""
+    output3.textContent = ""
+    output4.textContent = ""
+    output5.textContent = ""
+}
 }
